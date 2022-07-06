4651dac9b53a69f63d0b50ce0d03269d1c5f1c78
Qubi0-0 <kuba1102@gmail.com>
2022-07-06 17:33:38 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,24 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+
 BluetoothSerial SerialBT;
-
 Servo myservo;
-const int motor_speed = 255/2;
-int servo_position, duration, distance;
-const float distance_const = 0.034/2;
-String voice_command;
+String voice_command; 
 
-// the setup function runs once when you press reset or power the board
+
+/*setting pwm of the motor to 255
+we can change the speed of rotaion
+by chaning pwm input but we are only
+using arduino so we are using higest
+value to driver the motor  */ 
+const int motor_speed = 255/2;
+const float distance_const = 0.034/2;
+
+int servo_position, duration, distance;
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  // pinMode(LED_BUILTIN, OUTPUT);
   pinMode(pwm,OUTPUT) ;  	//we have to set PWM pin as output
   pinMode(in_1,OUTPUT) ; 	//Logic pins are also set as output
   pinMode(in_2,OUTPUT) ;  // Sets 
@@ -35,7 +41,7 @@ void setup() {
   myservo.attach(servo_in);
   Serial.begin(115200);
   SerialBT.begin("RC_Car"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  Serial.println("The device started, now pair it with bluetooth!");
 
 
 }
@@ -71,12 +77,12 @@ void loop() {
         digitalWrite(in_2,LOW) ;
         analogWrite(pwm,motor_speed) ; }
       else if (voice_command == "left") {
-        myservo.write(servo_position == 30);
+        myservo.write(servo_position = 30);
         delay(20);
         digitalWrite(in_1,HIGH) ;
         digitalWrite(in_2,HIGH) ; }
       else if (voice_command == "right") {
-        myservo.write(servo_position == 90);
+        myservo.write(servo_position = 90);
         delay(20);
         digitalWrite(in_1,HIGH) ;
         digitalWrite(in_2,HIGH) ; }
@@ -89,45 +95,6 @@ void loop() {
       }
 
   }
-
-
-
-
-
-
-
-
-
-// val = 30;     // scale it to use it with the servo (value between 0 and 180)
-   
-/*setting pwm of the motor to 255
-we can change the speed of rotaion
-by chaning pwm input but we are only
-using arduino so we are using higest
-value to driver the motor  */ 
-
-
-//For brake
-digitalWrite(in_1,HIGH) ;
-digitalWrite(in_2,HIGH) ;
-delay(1000) ;
-// val = 90;     // scale it to use it with the servo (value between 0 and 180)
-// myservo.write(val); 
-//For Anti Clock-wise motion - IN_1 = LOW , IN_2 = HIGH
-digitalWrite(in_1,LOW) ;
-digitalWrite(in_2,HIGH) ;
-delay(3000) ;
-// val = 120;     // scale it to use it with the servo (value between 0 and 180)
-// myservo.write(val); 
-//For brake
-digitalWrite(in_1,HIGH) ;
-digitalWrite(in_2,HIGH) ;
-delay(1000) ;                  // wait for a second
-
-
-
-  Serial.print("Distance: ");
-  Serial.println(distance);
 
 
 
